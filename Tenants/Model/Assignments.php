528d7a89c5121748a9b375c0b80abe084688297b
Volodymyr Volynets <volodymyr.volynets@gmail.com>
2018-09-25 21:56:49 -0400

    Change Log, Tenants --- Change Log:	Bug \#1020 \(Updated change log module.\)Tenants:	Enhancement \#1041 \(Added assignments table.\)

---
+++ Tenants/Model/Assignments.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Numbers\Tenants\Tenants\Model;
+class Assignments extends \Object\Table {
+	public $db_link;
+	public $db_link_flag;
+	public $module_code = 'TM';
+	public $title = 'T/M Assignments';
+	public $name = 'tm_assignments';
+	public $pk = ['tm_assignment_id'];
+	public $tenant = false;
+	public $orderby;
+	public $limit;
+	public $column_prefix = 'tm_assignment_';
+	public $columns = [
+		'tm_assignment_id' => ['name' => 'Tenant #', 'domain' => 'tenant_id_sequence'],
+		'tm_assignment_code' => ['name' => 'Code', 'domain' => 'domain_part'],
+		'tm_assignment_name' => ['name' => 'Name', 'domain' => 'name'],
+		'tm_assignment_email' => ['name' => 'Primary Email', 'domain' => 'email', 'null' => true],
+		'tm_assignment_phone' => ['name' => 'Primary Phone', 'domain' => 'phone', 'null' => true],
+		'tm_assignment_inactive' => ['name' => 'Inactive', 'type' => 'boolean']
+	];
+	public $constraints = [
+		'tm_assignments_pk' => ['type' => 'pk', 'columns' => ['tm_assignment_id']],
+		'tm_assignment_code_un' => ['type' => 'unique', 'columns' => ['tm_assignment_code']],
+	];
+	public $indexes = [
+		'tm_assignments_fulltext_idx' => ['type' => 'fulltext', 'columns' => ['tm_assignment_code', 'tm_assignment_name']]
+	];
+	public $history = false;
+	public $audit = false;
+	public $optimistic_lock = true;
+	public $options_map = [
+		'tm_assignment_name' => 'name',
+		'tm_assignment_code' => 'name',
+		'tm_assignment_id' => 'name'
+	];
+	public $options_active = [
+		'tm_assignment_inactive' => 0
+	];
+	public $engine = [
+		'MySQLi' => 'InnoDB'
+	];
+
+	public $cache = true;
+	public $cache_tags = [];
+	public $cache_memory = false;
+
+	public $data_asset = [
+		'classification' => 'proprietary',
+		'protection' => 1,
+		'scope' => 'global'
+	];
+}
